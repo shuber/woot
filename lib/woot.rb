548b4db2b4d6e05ef91585762800f243f2da2bbb
Sean Huber <shuber@huberry.com>
2009-10-30 09:55:46 -0700

    remove scraper class

--- lib/woot.rb
+++ lib/woot.rb
@@ -1,37 +1,31 @@
-module Woot
+class Woot
   DOMAIN = 'woot.com'
   
   def self.scrape(subdomain = :www)
-    host = "http://#{subdomain}.#{DOMAIN}"
-    ::Scraper.define do
-      result *(Scraper.selectors.inject([]) do |array, (pattern, results)|
+    selectors = self.selectors(subdomain)
+    Scraper.define do
+      result *(selectors.inject([]) do |array, (pattern, results)|
         process pattern, results
         array += results.keys
       end)
-    end.scrape(URI.parse("#{host}/"))
+    end.scrape(URI.parse("http://#{subdomain}.#{DOMAIN}/"))
   end
   
-  class Scraper
-    def self.selector(pattern, results)
-      selectors[pattern] = results
-    end
-    
-    def self.selectors
-      @selectors ||= {}
-    end
-    
-    selector 'h2.fn', :title => :text
-    selector 'span.amount', :price => :text
-    selector 'ul#shippingOptions', :shipping => :text
-    selector 'img.photo', :image => '@src'
-    selector 'div.hproduct>a', :alternate_image => proc { |element| $1 if element.attributes['href'] =~ /\('([^']+)'\);/ }
-    selector 'a.url', :url => '@href'
-    selector 'a#ctl00_ctl00_ContentPlaceHolderLeadIn_ContentPlaceHolderLeadIn_SaleControl_HyperLinkWantOne', :purchase_url => '@href'
-    selector 'li.comments>a', :comments_url => '@href', :comments_count => proc { |element| element.children[0].content.gsub(/\D/, '') }
-    selector 'div.productDescription>dl', :details => :text
-    selector 'div.story>h2', :header => :text
-    selector 'div.story>h3', :sub_header => :text
-    selector 'div.writeUp', :writeup => :text
-    selector 'div.specs', :specs => :text
+  def self.selectors(subdomain = :www)
+    @selectors = {
+      'h2.fn'                     => { :title => :text },
+      'span.amount'               => { :price => :text },
+      'ul#shippingOptions'        => { :shipping => :text },
+      'img.photo'                 => { :image => '@src' },
+      'div.hproduct>a'            => { :alternate_image => proc { |element| $1 if element.attributes['href'] =~ /\('([^']+)'\);/ } },
+      'a.url'                     => { :url => '@href' },
+      'li.comments>a'             => { :comments_url => '@href', :comments_count => proc { |element| element.children[0].content.gsub(/\D/, '') } },
+      'div.story>h2'              => { :header => :text },
+      'div.story>h3'              => { :sub_header => :text },
+      'div.writeUp'               => { :writeup => :text },
+      'div.specs'                 => { :specs => :text },
+      'div.productDescription>dl' => { :details => :text },
+      'a#ctl00_ctl00_ContentPlaceHolderLeadIn_ContentPlaceHolderLeadIn_SaleControl_HyperLinkWantOne' => { :purchase_url => proc { |element| "http://#{subdomain}.#{DOMAIN}#{element.attributes['href']}" if element.attributes.has_key?('href') } }
+    }
   end
 end
