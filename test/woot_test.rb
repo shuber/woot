54bda8682e4f55cdca6708585223955e437cb0bd
Sean Huber <shuber@huberry.com>
2009-10-30 10:17:09 -0700

    add tests

--- test/woot_test.rb
+++ test/woot_test.rb
@@ -1,9 +1,28 @@
 require 'test_helper'
 
+def subdomains; [:www, :wine, :shirt, :kids]; end # TODO: sellout
+def possible_blanks; [:purchase_url]; end
+def attributes; Woot.selectors.map { |selector, results| results.keys }.flatten; end
+
 class WootTest < Test::Unit::TestCase
 
-  should 'test this gem' do
-    flunk
+  subdomains.each do |subdomain|
+    context "When parsing http://#{subdomain}.#{Woot::DOMAIN}/ it" do
+      setup { @woot = Woot.scrape(subdomain) }
+      
+      attributes.each do |attribute|
+        should "have a key for #{attribute}" do
+          assert @woot.members.include?(attribute.to_s)
+        end
+        
+        unless possible_blanks.include?(attribute)
+          should "have a value for #{attribute}" do
+            assert !@woot.send(attribute).nil?
+            assert !@woot.send(attribute).empty?
+          end
+        end
+      end
+    end
   end
   
 end
